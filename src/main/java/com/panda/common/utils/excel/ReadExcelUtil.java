540c5415e901f0dde682fe2303227d622b65cc01
AirDong <panda_dong@126.com>
2018-08-23 22:55:23 +0800

    'temp'

--- src/main/java/com/panda/common/utils/excel/ReadExcelUtil.java
+++ src/main/java/com/panda/common/utils/excel/ReadExcelUtil.java
@@ -97,9 +97,6 @@ public class ReadExcelUtil extends AbstractExcel{
         }
         return data;
     }
-    public static void main(String[] args)throws Exception{
-        File file = new File("/tmp/112.xlsx");
-        ReadExcelUtil.readXlsxSheetWithSax(file);
-    }
+
 
 }
